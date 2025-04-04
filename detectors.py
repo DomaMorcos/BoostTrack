38b1892494e61451e5c0a4b063600709fe09d043
DomaMorcos <domadios.morcos@gmail.com>
2025-04-04 02:31:01 +0200

    0.68 instead of 0.65

--- detectors.py
+++ detectors.py
@@ -59,7 +59,7 @@ class FasterRCNNDetector:
             rpn_anchor_generator=anchor_generator,
             rpn_pre_nms_top_n_test=1000,
             rpn_post_nms_top_n_test=300,
-            rpn_nms_thresh=0.65,
+            rpn_nms_thresh=0.68,
             box_score_thresh=0.05,
             box_nms_thresh=0.5,
             box_detections_per_img=300
